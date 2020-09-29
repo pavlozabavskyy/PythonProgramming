f928bd3f1b724645b352af51ea5317872886d21f
pavlozab <pavlo.zabavskiy@lnu.edu.ua>
2020-09-30 00:13:27 +0300

    added validation method

--- main.py
+++ main.py
@@ -39,30 +39,30 @@ def minElemAverage(arr):
     return index
 
 
-def sortElem(arr):
-    newArray = []
-
-    l, r = minElemAverage(arr), maxElemAverage(arr)
+def progTask(arr):
+    l, r, j = minElemAverage(arr), maxElemAverage(arr), 0
 
     if r < l:
         temp = l
         l = r
         r = temp
 
-    for i in range(l + 1, r):
-        newArray.append(arr[i])
-
-    print ("Elements in between (with reverse sort) - ", newArray)
-
-    newArray.sort(reverse=True)
-
-    j = 0
-    for i in range(l + 1, r):
-        arr[i] = newArray[j]
+    for i in range((r-l)//2):
+        temp = arr[l+j]
+        arr[l + j] = arr[r - j]
+        arr[r - j] = temp
         j += 1
 
     print ("Result : ", arr)
 
+def validationInt(name):
+    while True:
+        try:
+            n = int(input(name))
+            break
+        except ValueError:
+            print('Incorrect input')
+    return n
 
 def printOption():
     print ("\n Choose : \n '1' - create a new array  \n '2' -  create random array \n '3' - see present array")
@@ -70,13 +70,15 @@ def printOption():
 
     while True:
         try:
-            choice = int(input("Your choice - "))
+            choice = validationInt("Your choice - ")
             if choice < 1 or choice > 5:
                 raise Exception("Incorrect input")
             else:
                 return choice
         except Exception as e:
             print (e)
+
+
 
 
 def main():
@@ -87,21 +89,21 @@ def main():
 
         if choice == 1:
             try:
-                arrSize = int(input("Enter the size of a new array - "))
+                arrSize = validationInt("Enter the size of a new array - ")
                 if arrSize < 0:
                     raise Exception("Incorrect input")
                 else:
                     arr = []
                     for i in range(0, arrSize):
-                        arr.append(int(input("Enter element : ")))
+                        arr.append(validationInt("Enter element : "))
             except Exception as e:
                 print (e)
 
         elif choice == 2:
             try:
-                arrSize = int(input("Enter the size of a new array - "))
-                loEnd = int(input("Enter the lower end of random numbers - "))
-                hiEnd = int(input("Enter the higher end of random numbers - "))
+                arrSize = validationInt("Enter the size of a new array - ")
+                loEnd = validationInt("Enter the lower end of random numbers - ")
+                hiEnd = validationInt("Enter the higher end of random numbers - ")
                 if arrSize < 0 or hiEnd < loEnd:
                     raise Exception("Incorrect input")
                 else:
@@ -124,7 +126,7 @@ def main():
                 if len(arr) == 0:
                     raise Exception("No array ")
                 else:
-                    sortElem(arr)
+                    progTask(arr)
             except Exception as e:
                 print (e)
 
