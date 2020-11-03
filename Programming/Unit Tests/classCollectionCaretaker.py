287e5e7ff69345a9d7d26268e55c68d2a7527802
pavlozab <pavlo.zabavskiy@lnu.edu.ua>
2020-11-03 21:44:54 +0200

    Unit Tests

---
+++ Programming/Unit Tests/classCollectionCaretaker.py
@@ -0,0 +1,77 @@
+from classCollection import CollectionAddress
+from classCollectionMemento import CollectionMemento
+from decorator import *
+
+class CollectionCaretaker:
+
+    def __init__(self, collect):
+        self._collect = collect
+        self._mementos = []
+        self._current = -2
+        self._maxlen = 5
+        self._checkUndo = False
+
+    def backup(self):
+        print('\nCaretaker: saving collection...')
+        self._mementos.append(self._collect.save())
+        self._maxlen += 1
+        if not self._checkUndo:
+            self._current += 1
+        if self._maxlen == 6:
+            self._mementos.pop(0)
+            #self._current -= 1
+
+    def __isEmpty(self):
+        if not len(self._mementos):
+            raise Exception('empty')
+
+    def undo(self):
+        self.__isEmpty()
+
+        if self._current == 0:
+            raise Exception('Current = 0')
+            
+        else:
+            self._current -= 1
+
+            print(self._current)
+            memento = self._mementos[self._current]
+
+            try:
+                self._collect.restore(memento)
+            except Exception as e:
+                print(e)
+
+            self._checkUndo = True
+
+    def redo(self):
+        self.__isEmpty()
+
+        if self._maxlen == 5:
+            raise Exception('last caretaker')        
+
+        else:
+            self._current += 1
+            memento = self._mementos[self._current]
+            print(self._current)
+            try:
+                self._collect.restore(memento)
+            except Exception as e:
+                print(e)
+        
+
+    def show_history(self):
+        print('\n\n!','_'*30 ,'history','_'*30, '!', "\nCaretaker: Here's the list of mementos:")
+        j = 0
+        for memento in self._mementos:
+            print(j, ' -- memento', '-'*40, '\n')
+            print(memento.name)
+            j+=1
+        print('!','_'*30 ,'history','_'*30, '!\n\n')
+
+    @property
+    def collect(self):
+        return self._collect
+
+   
+
---
+++ Programming/Unit Tests/programmingTask6.py
@@ -0,0 +1,94 @@
+from classAddress import Address as CA
+from classCollection import CollectionAddress as CC
+from classCollectionCaretaker import CollectionCaretaker
+from decorator import *
+
+def main():
+    options = '1 - insert collection from file\n2 - print collection\n3 - sort\n4 - search\n5 - delete obj\n6 - add new obj\n7 - edit obj\n8 - show history\n9 - undo\n10 - redo\n11 - exit\n'
+    collect = CC()
+    caretaker = CollectionCaretaker(collect)
+    while True:
+        try:
+            print(options)
+            choice = enterIntInRange(0, 'Enter choice : ', 0, 11)
+
+            if choice == 1:
+                menu1(caretaker, collect)
+
+            elif choice == 2:
+                print(collect)
+
+            elif choice == 3:
+                menu3(caretaker, collect)
+
+            elif choice == 4:
+                menu4(collect)
+
+            elif choice == 5:
+                menu5(caretaker, collect)
+
+            elif choice == 6:
+                menu6(caretaker, collect)
+
+            elif choice == 7:
+                menu7(caretaker, collect)
+
+            elif choice == 8:
+                menu8(caretaker)
+
+            elif choice == 9:
+                menu9(caretaker, collect)
+            
+            elif choice == 10:
+                menu10(caretaker, collect)
+                
+            elif choice == 11:
+                break
+
+        except Exception as e:
+            print('Error ', '--'*20, '  ',e)
+
+def menu1(caretaker, collect):
+    caretaker.backup()
+    collect.readJsonFile('data.json')
+    collect.writeJsonFile("output.json")
+
+def menu3(caretaker, collect):
+    attr = enterStr('0', 'Enter attribute: ')
+    caretaker.backup()
+    collect.sort(attr)
+    collect.writeJsonFile("output.json")
+
+def menu4(collect):
+    value = enterStr('0', "Enter search elem - ")
+    collect.search(value)
+
+def menu5(caretaker, collect):
+    index = enterIntInRange(0, f'Enter index from 0 to {len(collect) - 1}: ', 0, len(collect) -1)
+    caretaker.backup()
+    collect.deleteElem(index)
+    collect.writeJsonFile("output.json")
+
+def menu6(caretaker, collect):
+    caretaker.backup()
+    collect.addNewAddress()
+    collect.writeJsonFile("output.json")
+
+def menu7(caretaker, collect):
+    caretaker.backup()
+    collect.editAddress()
+    collect.writeJsonFile("output.json")
+
+def menu8(caretaker):
+    caretaker.show_history()
+
+def menu9(caretaker, collect):
+    caretaker.undo()
+    collect.writeJsonFile("output.json")
+
+def menu10(caretaker, collect):
+    caretaker.redo()
+    collect.writeJsonFile("output.json")
+
+if __name__ == '__main__':
+    main()
