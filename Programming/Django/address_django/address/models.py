8f7216a23e65021e5316e2b5860e66c3bf5d1e3c
pavlozab <pavlo.zabavskyi@lnu.edu.ua>
2020-11-16 22:14:32 +0200

    An attempt to correct validation

--- Programming/Django/address_django/address/models.py
+++ Programming/Django/address_django/address/models.py
@@ -1,10 +1,20 @@
 from django.db import models
-
+from django.core.validators import RegexValidator
 
 class Address(models.Model):
-    address_line = models.CharField(max_length=50)    # fix validation  (=
-    postal_code = models.IntegerField()
-    country = models.CharField(max_length=40)
-    city = models.CharField(max_length=40)
-    fax_number = models.CharField(max_length=17)
-    phone_number = models.CharField(max_length=17)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{12,15}$', 
+                                message="Phone number must be entered in the format: '+123456789012'.")
+    
+    postal_regex = RegexValidator(regex=r'^\d{5}$', 
+                                message='Postal code must be contains exactly 5 digits')
+
+    symbol_digits_regex = RegexValidator(regex=r'', 
+                                message='Dont use symbol')              #      fix it =( !!!
+
+
+    address_line = models.CharField(validators=[symbol_digits_regex], max_length=50)    # fix validation  (=
+    postal_code = models.CharField(validators=[postal_regex], max_length=5)
+    country = models.CharField(validators=[symbol_digits_regex], max_length=50)
+    city = models.CharField(validators=[symbol_digits_regex], max_length=50)
+    fax_number = models.CharField(validators=[phone_regex], max_length=13)
+    phone_number = models.CharField(validators=[phone_regex], max_length=13)
