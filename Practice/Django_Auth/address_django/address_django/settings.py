e348366df380ae59d320b87d7e30b9acce7ff9a1
pavlozab <pavlo.zabavskyi@lnu.edu.ua>
2020-11-26 11:58:22 +0200

    startapp account

--- Practice/Django_Auth/address_django/address_django/settings.py
+++ Practice/Django_Auth/address_django/address_django/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django_filters',
     
     'address',
+    'account',
 
     'rest_framework',
     'rest_framework.authtoken', 
@@ -82,8 +83,8 @@ WSGI_APPLICATION = 'address_django.wsgi.application'
 #
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 
